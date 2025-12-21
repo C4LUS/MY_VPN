6158b57322d0ef7dce67433ded53883971ae0350
C4LUS <lucas1.martin@epitech.eu>
2025-12-22 03:01:33 +0400

    add client code

--- main.c
+++ main.c
@@ -2,25 +2,18 @@
 
 int func(int connfd, char *buff, size_t buff_size)
 {
-    // Nettoie le buffer
+   
     bzero(buff, buff_size);
 
-    // Lecture depuis le client
     ssize_t r = read(connfd, buff, buff_size - 1);
     if (r <= 0) {
-        // r == 0  → déconnexion propre
-        // r < 0   → erreur
         printf("Error on read.\n");
         return -1;
     }
 
-    // On s'assure que buff soit une string
     buff[r] = '\0';
     printf("From client (%d): %s\n", connfd, buff);
 
-    // ------------------------------
-    //   RÉPONSE AU CLIENT (écho)
-    // ------------------------------
     if (write(connfd, buff, r) <= 0)
         return -1;
 
